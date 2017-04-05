8ad0a3b8e4d14539dfbb5f19d01cc616a965ce63
U-ED\dross11 <dross11@MVM-IGMM-L025.med.ed.ac.uk>
2017-04-05 16:21:05 +0100

    constructing plots

--- test/allele_balance.py
+++ test/allele_balance.py
@@ -1,12 +1,16 @@
 from pdVCF.pdVCF import VCF
 import copy
+import pandas as pd
 
-f = '/home/david/projects/scripts_cluster/projects/comparing_fluidigm_variants/vcfs/var.both.taadUkJan2017.filters.vcf'
+f = 'var.both.taadUkJan2017.filters.vcf'
 vcf = VCF(f)
 
 
 #AB_list = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
 AB_list = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+
+
+data = []
 
 for ab in AB_list:
     v = copy.deepcopy(vcf)
@@ -23,4 +27,8 @@ for ab in AB_list:
     else:
         print("0 variants after filtering for AB between {}-{}".format(ab_min, ab_max))
 
+    data.append([ab, v.vcf.shape[0], not_in_DB])
 
+
+df = pd.DataFrame(columns=['AB', 'No. Variants', 'dbSNP'],
+                  data=data)
