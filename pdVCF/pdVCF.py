31f81c06db16fa5ba79b05f83ea6f39882ed2136
david <dross78375@gmail.com>
2017-04-08 08:41:12 +0100

    Added error checking

--- pdVCF/pdVCF.py
+++ pdVCF/pdVCF.py
@@ -119,9 +119,17 @@ class VCF(object):
         fields = [x.split(" ")[0] for x in l]
         info = self.vcf.INFO.columns.tolist() 
         geno = ['DP', 'GT', 'AD', 'GQ', 'PL', 'AB']
+        mandatory = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER']
         
-        if len(set(fields).intersection(geno) ) > 0 and  len(set(fields).intersection(info) ) > 0:
+        if len(set(fields).intersection(geno) ) > 0 and len(set(fields).intersection(info) ) > 0:
             raise ValueError("Elements in {} are present in both the genotype and INFO fields. Filter genotype and INFO fields seperately.".format(fields))
+        
+        if len(set(fields).intersection(geno) ) > 0 and len(set(fields).intersection(mandatory) ) > 0:
+            raise ValueError("Elements in {} are present in both the genotype and mandatory fields. Filter genotype and mandatory fields seperately.".format(fields))
+
+        if len(set(fields).intersection(info) ) > 0 and len(set(fields).intersection(mandatory) ) > 0:
+            raise ValueError("Elements in {} are present in both the INFO and mandatory fields. Filter mandatory and INFO fields seperately.".format(fields))
+
 
 
 
